2dd4337aefa7929724a0de38f4d0bd01677763b5
Tim Jiang <lamec125910@gmail.com>
2024-10-12 02:53:59 +0800

    feat:add Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,16 +55,31 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+
+
+    // Compose
+    val composeUIVersion = "1.7.3"
+    implementation("androidx.compose.ui:ui:${composeUIVersion}")
+    implementation("androidx.compose.ui:ui-graphics:${composeUIVersion}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${composeUIVersion}")
+    implementation("androidx.compose.material3:material3:1.3.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${composeUIVersion}")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    debugImplementation("androidx.compose.ui:ui-tooling:${composeUIVersion}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${composeUIVersion}")
+
+    // CameraX
+    val cameraxVersion = "1.3.4"
+    implementation ("androidx.camera:camera-core:$cameraxVersion")
+    implementation ("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation ("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation ("androidx.camera:camera-view:1.3.4")
+
+    implementation ("com.google.android.material:material:1.12.0")
 
 }
